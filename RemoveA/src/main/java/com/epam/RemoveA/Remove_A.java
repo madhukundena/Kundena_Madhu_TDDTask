2daee8e57465acc23f4fa8283e281b9de17bf72c
Madhu Kundena <55804416+madhukundena@users.noreply.github.com>
2020-07-24 20:29:00 +0530

    Add files via upload

---
+++ RemoveA/src/main/java/com/epam/RemoveA/Remove_A.java
@@ -0,0 +1,43 @@
+package com.epam.RemoveA;
+
+public class Remove_A {
+	public String removeA(String str) {
+		String newStr = "";
+		if(str.length()<=2) {
+			if(str.length()<=1) {
+				if(str.charAt(0)=='A' || str.charAt(0)==' ') {
+					newStr=" ";
+				}
+				else {
+					newStr=str;
+				}
+			}
+			else {
+				if(str.charAt(0)=='A' && str.charAt(1)=='A') {
+					newStr=" ";
+				} else if(str.charAt(0)=='A')	{
+					newStr=str.substring(1);
+				} else if(str.charAt(1)=='A') {
+					newStr=str.substring(0,1);
+				} else{
+					newStr=str;
+				}
+			}
+		} else {
+			if(str.charAt(0)=='A' && str.charAt(1)=='A') {
+				newStr=str.substring(2);
+		      }
+		      else if(str.charAt(0)=='A') {
+			    newStr=str.substring(1);
+		      }
+		      else if(str.charAt(1)=='A') {
+			    newStr=str.charAt(0)+str.substring(2);
+		      }
+		      else {
+			    newStr=str;
+		      }
+	    }
+		return newStr;	
+		
+	}
+}
